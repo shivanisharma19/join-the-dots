76fb8e42b386dd07e413835eb1697080864a3501
Shivani Sharma <external.shivani.sharma@kmon.net>
2023-08-27 15:36:36 +0530

    Created welcome page added react router

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import { useNavigate} from 'react-router-dom'
 
 function App() {
+ const navigate = useNavigate()
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => navigate({to:'/'})}> Join the Dots </button>
       </header>
     </div>
   );
